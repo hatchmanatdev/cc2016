21934a618df7a147351bde0a602fb3e439d49188
hatchmanatdev <s0559271@htw-berlin.de>
2016-12-09 07:14:43 -0800

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ http.createServer(function (request, response) {
 
         // Send the response body as "Hello Cloud"
 
-        response.end('Hello Cloud\, I am process no'+procID); 
+        response.end('Hello Cloud\, I am process no: '+procID); 
     
 
    } 
