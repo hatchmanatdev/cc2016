4e4f03f4fb4a5acfabd631c30be2b62344099e79
hatchmanatdev <s0559271@htw-berlin.de>
2016-12-09 06:46:02 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,40 +1,36 @@
 var http = require('http');
-var url = require('url');
+
+
 
 http.createServer(function (request, response) {
+
    // Send the HTTP header 
+
    // HTTP Status: 200 : OK
+
    // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain', 'Autorization': 'auth'});
-   
-    var auth = request.headers['authorization'];
-    var queryData = url.parse(request.url, true).query;
-    var firstname =  queryData.fn;
-    var lastname =  queryData.ln;
+
+   response.writeHead(200, {'Content-Type': 'text/plain'});
     
-    var tmp = auth.split(' ');
-    var buf = new Buffer(tmp[1], 'base64');
-    var plain_auth = buf.toString();
+     var procID = process.pid;
     
-    var cred = plain_auth.split(':');
-    var username = cred[0];
-    var password = cred[1];
+   if(request.method=='POST') {
+
+       // Send the response body as "Hello Cloud"
+
+        response.end('Wow, this was an awesome POST request!\n'); 
+
+   }
+
+   else
+
+   {
+
+        // Send the response body as "Hello Cloud"
+
+        response.end('Hello Cloud\n'+ ', ProcID: '+procID); 
     
 
-   if(request.method=='POST') 
-   {
-        var body = [];
-        request.on('data', function(chunk) {
-            body.push(chunk);
-        }).on('end', function() {
-            body = Buffer.concat(body).toString();
-                // at this point, `body` has the entire request body stored in it as a string
-                response.end('Hi '+firstname+' '+lastname+'. This was an awesome request with query arguments! This is your provided e-mail address: '+body+', password: '+password); 
-            });       
-    }
-    else
-   {       
-        response.end('Wow, this was an awesome '+request.method+' request. But you were requestesd to make a POST request.!\n'); 
-   }
-   
+   } 
+
 }).listen(process.env.PORT);
